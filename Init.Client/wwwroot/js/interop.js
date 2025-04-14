1b9b4a709b43e733ceeb9552c6917795331ad00f
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-14 21:39:35 +0300

    registered search shortcut fo invoke csharp in interop.js

--- Init.Client/wwwroot/js/interop.js
+++ Init.Client/wwwroot/js/interop.js
@@ -12,3 +12,13 @@ window.focusElementById = function (id) {
         el.focus();
     }
 };
+
+window.registerSearchShortcut = function (dotNetHelper) {
+    document.addEventListener('keydown', function (e) {
+        const isTyping = document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA';
+        if (e.key === '/' && !isTyping) {
+            e.preventDefault();
+            dotNetHelper.invokeMethodAsync('ShowModal');
+        }
+    });
+};
