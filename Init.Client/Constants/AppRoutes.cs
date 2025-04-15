3fd082583e64edbdb5bbd9198b79aab191b93d0c
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-15 17:46:06 +0300

    included app routes constant file

---
+++ Init.Client/Constants/AppRoutes.cs
@@ -0,0 +1,9 @@
+﻿namespace Init.Client.Constants;
+
+public static class AppRoutes
+{
+    public const string Home = "/";
+    public const string About = "/about";
+    public const string Services = "/services";
+    public const string Contact = "/contact";
+}
