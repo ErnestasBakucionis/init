d7be0ee556e77cb0ee5b49a4db42ab12a2524e34
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-16 18:08:08 +0300

    added search result item structure

---
+++ Init.Client/Models/SearchResultItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Init.Client.Models;
+
+public class SearchResultItem
+{
+    public string Title { get; set; }
+    public string Path { get; set; }
+    public string Category { get; set; }
+}
