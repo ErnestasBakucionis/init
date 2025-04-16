9e0c7ef70aa67c0fbc16b773959c0438ee8d532a
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-16 18:08:31 +0300

    search data of pages and etc implemented

---
+++ Init.Client/Data/SearchData.cs
@@ -0,0 +1,22 @@
+﻿using Init.Client.Constants;
+using Init.Client.Models;
+
+namespace Init.Client.Data;
+
+public static class SearchData
+{
+    public static readonly List<SearchResultItem> AllItems = new()
+    {
+        // Pages
+        new() { Title = "Home", Path = AppRoutes.Home, Category = "Pages" },
+        new() { Title = "About", Path = AppRoutes.About, Category = "Pages" },
+        new() { Title = "Services", Path = AppRoutes.Services, Category = "Pages" },
+        new() { Title = "Contact", Path = AppRoutes.Contact, Category = "Pages" },
+
+        // FAQs (test)
+        new() { Title = "How to contact support?", Path = "/faq/contact-support", Category = "FAQs" },
+
+        // Articles (test)
+        new() { Title = "Getting Started with Our Services", Path = "/articles/getting-started", Category = "Articles" }
+    };
+}
