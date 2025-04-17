dacfd451dea0cb62abbb74e30213e4a2b0c5407f
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-17 22:32:30 +0300

    updated program.cs file created init of application

--- Init.Client/Program.cs
+++ Init.Client/Program.cs
@@ -1,16 +1,14 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using Init.Client.Services;
 using Init.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
 var serverUrl = builder.Configuration.GetValue<string>("ServerUrl");
-
 if (string.IsNullOrEmpty(serverUrl))
 {
     throw new InvalidOperationException("API Base URL is not configured in appsettings.json.");
@@ -18,4 +16,27 @@ if (string.IsNullOrEmpty(serverUrl))
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(serverUrl) });
 
-await builder.Build().RunAsync();
+builder.Services.AddHttpClient("FrontendClient", client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+
+builder.Services.AddSingleton<ITranslationService, TranslationService>();
+
+await InitializeAppAsync(builder);
+
+async Task InitializeAppAsync(WebAssemblyHostBuilder builder)
+{
+    var serviceProvider = builder.Services.BuildServiceProvider();
+    var translationService = serviceProvider.GetRequiredService<ITranslationService>();
+    var jsRuntime = serviceProvider.GetRequiredService<IJSRuntime>();
+
+    var lang = await jsRuntime.InvokeAsync<string>("localStorage.getItem", "lang") ?? "en";
+
+    await translationService.LoadTranslationsAsync(lang);
+
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+
+    await builder.Build().RunAsync();
+}
