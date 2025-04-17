c29f37417d46546cf0231a7530fb64ca1fb16e63
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-17 22:36:51 +0300

    updated search data with translations

--- Init.Client/Data/SearchData.cs
+++ Init.Client/Data/SearchData.cs
@@ -1,22 +1,28 @@
 ﻿using Init.Client.Constants;
 using Init.Client.Models;
+using Init.Client.Services;
 
 namespace Init.Client.Data;
 
 public static class SearchData
 {
-    public static readonly List<SearchResultItem> AllItems = new()
+    public static List<SearchResultItem> GetSearchItems(ITranslationService translationService)
     {
-        // Pages
-        new() { Title = "Home", Path = AppRoutes.Home, Category = "Pages" },
-        new() { Title = "About", Path = AppRoutes.About, Category = "Pages" },
-        new() { Title = "Services", Path = AppRoutes.Services, Category = "Pages" },
-        new() { Title = "Contact", Path = AppRoutes.Contact, Category = "Pages" },
+        var allItems = new List<SearchResultItem>
+        {
+            // Pages
+            new SearchResultItem { Title = translationService.Translate("HOME"), Path = AppRoutes.Home, Category = translationService.Translate("PAGES") },
+            new SearchResultItem { Title = translationService.Translate("ABOUT"), Path = AppRoutes.About, Category = translationService.Translate("PAGES") },
+            new SearchResultItem { Title = translationService.Translate("SERVICES"), Path = AppRoutes.Services, Category = translationService.Translate("PAGES") },
+            new SearchResultItem { Title = translationService.Translate("CONTACTS"), Path = AppRoutes.Contact, Category = translationService.Translate("PAGES") },
 
-        // FAQs (test)
-        new() { Title = "How to contact support?", Path = "/faq/contact-support", Category = "FAQs" },
+            // FAQs (test)
+            new SearchResultItem { Title = translationService.Translate("HOW_TO_CONTACT_SUPPORT"), Path = "/faq/contact-support", Category = "FAQs" },
 
-        // Articles (test)
-        new() { Title = "Getting Started with Our Services", Path = "/articles/getting-started", Category = "Articles" }
-    };
+            // Articles (test)
+            new SearchResultItem { Title = translationService.Translate("GETTING_STARTED"), Path = "/articles/getting-started", Category = "Articles" }
+        };
+
+        return allItems;
+    }
 }
