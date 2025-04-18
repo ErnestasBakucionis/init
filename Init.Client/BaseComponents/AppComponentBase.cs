c0e555d6d0f3f909833f5d7c6b238637a6473dbb
Ernestas <34349429+ErnestasBakucionis@users.noreply.github.com>
2025-04-18 17:33:52 +0300

    added app component base for translation injection and other injections in future

---
+++ Init.Client/BaseComponents/AppComponentBase.cs
@@ -0,0 +1,24 @@
+﻿using Init.Client.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Init.Client.BaseComponents;
+
+public abstract class AppComponentBase : ComponentBase, IDisposable
+{
+    [Inject] protected ITranslationService TranslateService { get; set; }
+
+    protected override void OnInitialized()
+    {
+        TranslateService.OnLanguageChanged += LanguageChanged;
+    }
+
+    private void LanguageChanged()
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    public virtual void Dispose()
+    {
+        TranslateService.OnLanguageChanged -= LanguageChanged;
+    }
+}
